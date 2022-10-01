2b89cf3524850caf14c3528284aeecdfa74c6f83
Dan Ward <danward@gmail.com>
2022-09-30 22:14:10 -0700

    Improve endpoint

--- pages/api/v1/nft/get-nft-info.js
+++ pages/api/v1/nft/get-nft-info.js
@@ -7,54 +7,64 @@ const ENV_URL = process.env.ENV_URL || "http://localhost:3000";
 const handler = async (req, res) => {
   const { mintAddress } = req.query;
 
+  const combindedData = {};
   try {
-    const combindedData = {};
-    try {
-      const { data: moonrank } = await axios.get(
-        `${ENV_URL}/api/v1/nft/get-moonrank-data?mintAddress=${mintAddress}`
-      );
-      // console.log("moonrank", moonrank);
-      combindedData.moonrank = moonrank;
-    } catch (error) {
-      console.error("error", error);
-    }
-    try {
-      const { data: metaplex } = await axios.get(
-        `${ENV_URL}/api/v1/nft/get-metaplex-data?mintAddress=${mintAddress}`
-      );
-      // console.log("metaplex", metaplex);
-      combindedData.metaplex = metaplex;
-    } catch (error) {
-      console.error("error", error);
-    }
-    try {
-      const { data: magicEden } = await axios.get(
-        `${ENV_URL}/api/v1/nft/get-magic-eden-data?mintAddress=${mintAddress}`
-      );
-      combindedData.magicEden = magicEden;
-      console.log("magicEden", magicEden);
-    } catch (error) {
-      console.error("error", error);
-    }
-
-    const { collection } = combindedData.magicEden;
-    console.log(collection);
-    if (collection) {
-      try {
-        const { data: howRare } = await axios.get(
-          `${ENV_URL}/api/v1/nft/get-how-rare-nft-data?collection=${collection}&mintAddress=${mintAddress}`
-        );
-        combindedData.howRare = howRare;
-        // console.log("howRare", howRare);
-      } catch (error) {
-        console.error("error", error);
-      }
-    }
-
-    res.status(200).json(combindedData);
+    const { data: moonrank } = await axios.get(
+      `${ENV_URL}/api/v1/nft/get-moonrank-data?mintAddress=${mintAddress}`
+    );
+    // console.log("moonrank", moonrank);
+    combindedData.moonrank = moonrank;
   } catch (error) {
-    console.log("error", error);
+    console.error("error", error);
   }
+  try {
+    const { data: metaplex } = await axios.get(
+      `${ENV_URL}/api/v1/nft/get-metaplex-data?mintAddress=${mintAddress}`
+    );
+    // console.log("metaplex", metaplex);
+    combindedData.metaplex = metaplex;
+  } catch (error) {
+    console.error("error", error);
+  }
+  try {
+    const { data: magicEden } = await axios.get(
+      `${ENV_URL}/api/v1/nft/get-magic-eden-data?mintAddress=${mintAddress}`
+    );
+    combindedData.magicEden = magicEden;
+    console.log("magicEden", magicEden);
+  } catch (error) {
+    console.error("error", error);
+  }
+
+  const { collection } = combindedData.magicEden;
+
+  if (collection) {
+    try {
+      const { data } = await axios.get(
+        `https://api.howrare.is/v0.1/collections/${collection}`
+      );
+
+      const { items } = data.result.data;
+      const howRare = items?.find((nft) => nft.mint === mintAddress);
+      combindedData.howRare = howRare;
+      // console.log("howRare", howRare);
+    } catch (error) {
+      console.error("error", error);
+    }
+
+    if (combindedData.howRare) return;
+    try {
+      const { data: dataFromSymbol } = await axios.get(
+        `https://api.howrare.is/v0.1/collections/${combindedData.metaplex.json.symbol}`
+      );
+      const { items } = dataFromSymbol.result.data;
+      const howRare = items?.find((nft) => nft.mint === mintAddress);
+      combindedData.howRare = howRare;
+    } catch (error) {
+      console.error("error", error);
+    }
+  }
+  res.status(200).json(combindedData);
 };
 
 export default handler;
