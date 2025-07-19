cc1f1cf42c855986097a87cfb47697a78131ea75
warly <warlyware@pm.me>
2025-07-19 13:53:19 -0700

    Complete working layout

--- components/project-card.jsx
+++ components/project-card.jsx
@@ -2,14 +2,51 @@ import React from "react";
 
 export default function ProjectCard({ project }) {
   return (
-    <div className="bg-cyan-200 rounded-lg shadow-md p-4 border-2 hover:border-pink-500 transition-all duration-300 cursor-pointer hover:scale-105">
-      <div className="flex items-center justify-between mb-2">
+    <div className="bg-cyan-200 rounded-lg shadow-md p-4 border-2 border-pink-400 text-purple-700">
+      <div className="flex items-center justify-between mb-1">
         <h3 className="text-lg font-bold text-purple-700 uppercase">{project.title}</h3>
         {project.years && (
-          <p className="text-sm text-purple-700">{project.years}</p>
+          <p className="text-purple-700">{project.years}</p>
         )}
       </div>
-      <p className="text-sm text-purple-700">{project.description}</p>
+      <div className="flex justify-between items-center w-full text-sm">
+        <p className="mb-2 italic">
+          {project.buildInfo}
+        </p>
+        {project.repoLinks && (
+          <div className="flex gap-2 mb-2">
+            <>
+              {Object.entries(project.repoLinks).map(([key, value]) => (
+                <a href={value}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={key}
+                  className="hover:text-pink-500 transition-all duration-300 underline"
+                >
+                  {key}
+                </a>
+              ))}
+            </>
+          </div>
+        )}
+        {project.links && (
+          <div className="flex gap-2 mb-2">
+            <>
+              {Object.entries(project.links).map(([key, value]) => (
+                <a href={value}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={key}
+                  className="hover:text-pink-500 transition-all duration-300 underline"
+                >
+                  {key}
+                </a>
+              ))}
+            </>
+          </div>
+        )}
+      </div>
+      <p className="text-sm mb-2">{project.description}</p>
       {project.techs && (
         <>
           <div className="flex justify-center w-full">
